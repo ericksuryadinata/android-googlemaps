aa653b3a74991c4d320db330d6447c246b665380
Erick Surya Dinata <ericksuryadinata@gmail.com>
2018-12-21 13:35:52 +0700

    change image

--- app/src/main/java/com/dariyanti/uasgis/MainActivity.java
+++ app/src/main/java/com/dariyanti/uasgis/MainActivity.java
@@ -1,15 +1,22 @@
 package com.dariyanti.uasgis;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +24,9 @@ import com.dariyanti.uasgis.Home.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView image_load_all, image_home;
+    private TextView text_home, text_load_all;
+    private LinearLayout ll_home, ll_load_all;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +35,38 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         initFragment(new HomeFragment());
+
+        ll_home = findViewById(R.id.ll_home);
+        ll_load_all = findViewById(R.id.ll_load_all);
+        image_home = findViewById(R.id.image_home);
+        image_load_all = findViewById(R.id.image_load_all);
+        text_home = findViewById(R.id.text_home);
+        text_load_all = findViewById(R.id.text_load_all);
+        final ColorStateList oldColors = text_home.getTextColors();
+        text_home.setTextColor(Color.parseColor("#FF0090"));
+        image_home.setPressed(true);
+
+        ll_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text_home.setTextColor(Color.parseColor("#FF0090"));
+                text_load_all.setTextColor(oldColors);
+                new Handler().post(new Runnable() {
+                    @Override
+                    public void run() {
+                        image_home.setPressed(true);
+                        image_load_all.setPressed(false);
+                    }
+                });
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                Fragment fragment = fragmentManager.findFragmentById(R.id.fl_main);
+                if(fragment != null && fragment instanceof HomeFragment){
+
+                }else{
+                    initFragment(new HomeFragment());
+                }
+            }
+        });
 
     }
 
--- app/src/main/java/com/dariyanti/uasgis/SplashScreenActivity.java
+++ app/src/main/java/com/dariyanti/uasgis/SplashScreenActivity.java
@@ -6,17 +6,26 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private Button btn_mulai;
+    private Button button_mulai;
+    private ImageView image_splash;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
-        btn_mulai=findViewById(R.id.btn_mulai);
-        btn_mulai.setOnClickListener(new View.OnClickListener() {
+        button_mulai=findViewById(R.id.button_mulai);
+        button_mulai.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToNextScreen();
