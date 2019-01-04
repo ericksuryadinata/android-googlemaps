e560c88f783588cd09f6b6a58741618b4e6eb310
Aditya Nanda <uananda10@gmail.com>
2019-01-04 18:54:28 +0700

    resolving conflict adit

--- app/src/main/java/com/dariyanti/uasgis/API/URL.java
+++ app/src/main/java/com/dariyanti/uasgis/API/URL.java
@@ -1,13 +1,7 @@
 package com.dariyanti.uasgis.API;
 
 public class URL {
-
-<<<<<<< HEAD
+    
     public static final String BASE_URL = "http://172.100.0.2/apigooglemaps/public/api/",
             MAIN_URL = "students";
-=======
-    public static final String URL= "http://192.168.43.208/REST_MAP/", BASE_URL = URL + "index.php/",
-            //BASE_URL = "http://192.168.1.141/REST_MAP/index.php/",
-            MAIN_URL = "Map", MAHASISWA_IMAGE = "assets/mahasiswa/";
->>>>>>> 44414f282208ac9cfa0f92a85bf645ceb712fabb
 }
