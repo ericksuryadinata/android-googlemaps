6353a35a940b27bced678a4c2d6cc68ab82f06fd
Aditya Nanda <uananda10@gmail.com>
2019-01-05 17:06:28 +0700

    Web tiremenggal

--- app/src/main/java/com/dariyanti/uasgis/API/URL.java
+++ app/src/main/java/com/dariyanti/uasgis/API/URL.java
@@ -2,8 +2,10 @@ package com.dariyanti.uasgis.API;
 
 public class URL {
 
-    public static final String BASE_URL = "http://192.168.43.41/apigooglemaps/",
-            API_URL = BASE_URL+"public/api/",
+    public static final String
+            //BASE_URL = "http://192.168.43.41/apigooglemaps/",
+            BASE_URL = "http://mapsapi.tiremenggal.com/",
+            API_URL = BASE_URL+"api/",
             MAIN_URL = API_URL+"students/",
             MAHASISWA_IMAGE  = BASE_URL+"public/images/";
 }
