341a5560909b54a6d1778036a482e24e99eb9c74
Erick Surya Dinata <ericksuryadinata@gmail.com>
2019-02-05 08:11:51 +0700

    change url

--- app/src/main/java/com/dariyanti/uasgis/API/URL.java
+++ app/src/main/java/com/dariyanti/uasgis/API/URL.java
@@ -4,7 +4,7 @@ public class URL {
 
     public static final String
 //            BASE_URL = "http://192.168.43.44/apigooglemaps/public/",
-            BASE_URL = "http://mapsapi.tiremenggal.com/",
+            BASE_URL = "http://maps.tiremenggal.com/",
             API_URL = BASE_URL+"api/",
             MAIN_URL = API_URL+"students",
             MAHASISWA_IMAGE  = BASE_URL+"images/";
